58848c45b7c1e67d462f941bd333c08fd660f000
Fabian Grewing <fabian.heimbuerger@gmail.com>
2023-06-13 11:12:51 +0200

    Add destructor to HttpContentWrapper

--- src/Grpc.Net.Client/Balancer/Internal/HttpContentWrapper.cs
+++ src/Grpc.Net.Client/Balancer/Internal/HttpContentWrapper.cs
@@ -1,3 +1,21 @@
+#region Copyright notice and license
+
+// Copyright 2019 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
 using System.Net;
 
 namespace Grpc.Net.Client.Balancer.Internal;
@@ -17,6 +35,11 @@ internal sealed class HttpContentWrapper : HttpContent
         {
             Headers.TryAddWithoutValidation(kvp.Key, kvp.Value.ToArray());
         }
+    }
+
+    ~HttpContentWrapper()
+    {
+        Dispose(false);
     }
 
 #if NET5_0_OR_GREATER
@@ -59,11 +82,15 @@ internal sealed class HttpContentWrapper : HttpContent
     {
         base.Dispose(disposing);
 
-        if (disposing && !_disposed)
+        if (!_disposed)
         {
             _disposeAction();
-            _inner.Dispose();
             _disposed = true;
+        }
+
+        if (disposing)
+        {
+            _inner.Dispose();
         }
     }
 }
